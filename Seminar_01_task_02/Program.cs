c5eb7ac6acda248b4ccb671bd422be2e6d4d2da8
draguanna <draguanna@yandex.ru>
2023-09-07 17:07:37 +0300

    Added homework for seminar 01 tasks 02, 04, 06, 08

---
+++ Seminar_01_task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Домашнее задание к семинару 01
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear(); // Очистка терминала
+int firstNum, secondNum; // Объявление целочисленных переменных
+
+// Ввод пользователем первого числа
+Console.Write("Введите первое число: ");
+while (!(int.TryParse(Console.ReadLine(), out firstNum)))
+     {Console.Write("Введено не целое число, попробуйте еще раз: ");}
+
+// Ввод пользователем второго числа
+Console.Write("Введите второе число: ");
+while (!(int.TryParse(Console.ReadLine(), out secondNum)))
+    {Console.Write("Введено не целое число, попробуйте еще раз: ");}
+
+//Вывод сведений о минимальном и максимальном числе
+if (firstNum > secondNum) {Console.Write($"max = {firstNum}, min = {secondNum}");}
+    else if (firstNum < secondNum) {Console.Write($"max = {secondNum}, min = {firstNum}");}
+        else {Console.Write($"max = min = {firstNum}");}
---
+++ Seminar_01_task_04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашнее задание к семинару 01
+// Задача 4:Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear(); // Очистка терминала
+int [] numbArray = new int[3]; // Объяление массива
+
+// Ввод чисел пользователем
+Console.WriteLine("Введите поочередно три целых числа.");
+for(int i = 0; i < numbArray.Length; i++)
+{
+    Console.Write($"Число[{i}]= ");
+    while (!(int.TryParse(Console.ReadLine(), out numbArray[i])))
+    {Console.Write("Введено не целое число, попробуйте еще раз: ");}
+}
+Console.Write($"max = "+ numbArray.Max());
+
