9c6b239fb114c4df2d71d49dedc4ca11d8a91ec8
draguanna <draguanna@yandex.ru>
2023-09-19 09:56:39 +0300

    Added table view in task 22

--- Seminar_03_task_22/Program.cs
+++ Seminar_03_task_22/Program.cs
@@ -3,12 +3,9 @@
 //   Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
 //------------------------------------------------------------------------------------------------------------------
 
+//Методы
 
-//------------------------------------------------------------------------------------------------------------------
-//                                                  Методы
-//------------------------------------------------------------------------------------------------------------------
-
-//Ввод данных
+//Ввод данных пользователем с клавиатуры
 int ReadData(string msg)
 {
     Console.WriteLine(msg);
@@ -16,26 +13,71 @@ int ReadData(string msg)
     return num;
 }
 
-// Получение строки
+// Получение строки с числами в нужной степени
 string BuildLine(int num, int pow)
 {
     string res = string.Empty;
-    for (int i = 1; i <= num; i++) { res = res + Math.Pow(i, pow) + " ";}
+    for (int i = 1; i <= num; i++) { res+= String.Format("{0,5}\u2502", Math.Pow(i, pow)); }
     return res;
 }
 
-//Вывод данных
-void PrintResult(string msg)
+// Нарисовать линию таблицы
+void PrintLine(int count, string type)
 {
-    Console.WriteLine(msg);
+    string line = string.Empty;
+    char firstChar = '+';
+    char middleChar = '+';
+    char lastChar = '+';
+
+    //Определить набор символов в линии в зависимости от ее положения
+    if (type == "top")  // Если линия сверху таблицы
+    {
+        firstChar = '\u250C';  // ┌
+        middleChar = '\u252C'; // ┬
+        lastChar = '\u2510';   // ┐
+    }
+    if (type == "inside") // Если линия внутри таблицы
+    {
+        firstChar = '\u251C';  // ├
+        middleChar = '\u253C'; // ┼
+        lastChar = '\u2524';   // ┤
+    }
+      if (type == "bottom") // Если линия внизу таблицы
+    {
+        firstChar = '\u2514';  // └
+        middleChar = '\u2534'; // ┴
+        lastChar = '\u2518';   // ┘
+    }
+
+    line += firstChar;   // Установка первого символа в зависимости от положения линии в таблице
+    for (int i = 0; i < 13; i++) { line += "\u2500"; } // Обрамление наименования строк.
+
+    // Обрамление вдоль столбцов 
+    for (int i = 0; i < count; i++)
+    {
+        line += middleChar;
+        for (int j = 0; j < 5; j++) { line += "\u2500"; }
+    }
+    
+    line+=lastChar;          // Установка последнего символа в зависимости от положения линии в таблице
+    Console.WriteLine(line); // Печать собранной линии
 }
 
-//------------------------------------------------------------------------------------------------------------------
-//                                               Тело программы
-//------------------------------------------------------------------------------------------------------------------
+//Вывод строки с числами
+void PrintResult(string rowName, string rowString)
+{
+    Console.WriteLine($"\u2502{rowName,13}\u2502{rowString}");
+}
+
+//Тело программы
+
 Console.Clear(); // Очистка терминала
 int number = ReadData("Введите положительное число N");
-string sourceString = BuildLine(number,1);
-string resultString = BuildLine(number,2);
-PrintResult(sourceString + "-> " + resultString);
+string sourceString = BuildLine(number, 1);
+string resultString = BuildLine(number, 2);
 
+PrintLine(number, "top");
+PrintResult ("Число",sourceString );
+PrintLine(number, "inside");
+PrintResult ("Квадрат числа", resultString );
+PrintLine(number, "bottom");
