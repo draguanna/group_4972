8ff15fb7813634993f7342becb9ac13d70fdcaaa
draguanna <draguanna@yandex.ru>
2023-09-19 19:53:05 +0300

    Added solutions fot homework tasks 25, 27 from seminar 04

---
+++ Seminar_04_task_25/Program.cs
@@ -0,0 +1,91 @@
+﻿//------------------------------------------------------------------------------------------------------------------
+//                                                  Задача 25
+//                                      Домашнее задание к семинару 04
+//   Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//   * Написать калькулятор с операциями +, -, /, * и возведение в степень
+//------------------------------------------------------------------------------------------------------------------
+
+//                                                 Тело программы
+//------------------------------------------------------------------------------------------------------------------
+Console.Clear(); // Очистка консоли
+
+// Ввод выражения c клавиатуры
+int numA = ReadInt("Введите число A: ");
+char operation = ReadOperation("Введите оператор (+, -, *, /, ^): ");
+int numB = ReadInt("Введите число B: ");
+
+double result = Calculate(numA, numB, operation); // Вычисление результата
+PrintResult($"{numA}{operation}{numB}={result}"); // Вывод результата на экран
+
+//                                                    Методы
+//------------------------------------------------------------------------------------------------------------------
+// Ввод числа с клавиатуры
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Ввод оператора с клавиатуры
+char ReadOperation(string msg)
+{
+    Console.Write(msg);
+    return char.Parse(Console.ReadLine() ?? "+"); 
+}
+
+// Вывод результата на экран
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Сумма
+int Sum(int numA, int numB)
+{
+    int sum = numA + numB;
+    return sum;
+}
+
+// Разность
+int Subtraction(int numA, int numB)
+{
+    int sub = numA - numB;
+    return sub;
+}
+
+// Деление
+double Division(int numA, int numB)
+{
+    double div = (double)numA / numB;
+    return div;
+}
+
+// Умножение 
+int Multiplication(int numA, int numB)
+{
+    int mult = numA * numB;
+    return mult;
+}
+// Возведение в степень
+long Pow(int numA, int numB)
+{
+    long exp = 1;
+    for (int i = 1; i <= numB; i++) { exp *= numA; }
+    return exp;
+}
+
+// Вычисление результата
+double Calculate(int numA, int numB, char operation)
+{
+    double result = 0;
+    switch (operation)
+    {
+        case '+': result = Sum(numA, numB); break;
+        case '-': result = Subtraction(numA, numB); break;
+        case '/': result = Division(numA, numB); break;
+        case '*': result = Multiplication(numA, numB); break;
+        case '^': result = Pow(numA, numB); break;
+        default: return 0;
+    }
+    return result;
+}
---
+++ Seminar_04_task_27/Program.cs
@@ -0,0 +1,56 @@
+﻿//------------------------------------------------------------------------------------------------------------------
+//                                                  Задача 27
+//                                      Домашнее задание к семинару 04
+//   Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе..
+//   * Сделать оценку времени алгоритма через вещественные числа и строки
+//------------------------------------------------------------------------------------------------------------------
+
+//                                                 Тело программы
+//------------------------------------------------------------------------------------------------------------------
+Console.Clear(); // Очистка консоли
+int number = ReadData("Введите целое число: "); // Ввод числа с клавиатуры
+
+CalculateAndPrintSum("Метод деления", number, SumDigitDiv); // Сумма методом деления
+CalculateAndPrintSum("Метод строки", number, SumDigitStr);  // Сумма методом строки
+
+
+//                                                     Методы
+//------------------------------------------------------------------------------------------------------------------
+// Ввод данных с клавиатуры
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+// Вывод результата
+void CalculateAndPrintSum(string methodName, int number, Func<int, int> calculator)
+{
+    DateTime startTime = DateTime.Now;
+    int sum = calculator(number);
+    TimeSpan executionTime = DateTime.Now - startTime;
+    Console.WriteLine($"\n{methodName}:\nСумма цифр числа {number} = {sum}\nВремя исполнения {executionTime}");
+}
+
+// Вычисление суммы цифр через вещественные числа
+int SumDigitDiv(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+//Вычисление суммы цифр через строку
+int SumDigitStr(int number)
+{
+    string numberStr = number.ToString();
+    int result = 0;
+
+    foreach (char digitChar in numberStr) { result += int.Parse(digitChar.ToString()); }
+    return result;
+}
