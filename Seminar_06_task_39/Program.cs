359ed7e5b6c8d0978ea5d8ed369649f069a87fe2
draguanna <draguanna@yandex.ru>
2023-09-22 22:24:36 +0300

    Added solutions for tasks 39, 40, 42, 44, 45 from seminar 06

---
+++ Seminar_06_task_39/Program.cs
@@ -0,0 +1,64 @@
+﻿//------------------------------------------------------------------------------------------------------------------
+//                                                  Задача 39
+//   Напишите программу, которая перевернёт одномерныймассив 
+//   (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+//------------------------------------------------------------------------------------------------------------------
+
+//                                                Тело программы
+//------------------------------------------------------------------------------------------------------------------
+Console.Clear();
+
+// Генерация массива из случайных чисел и вывод
+int[] array = GenerateRandomArray(10, 0, 9);
+PrintArray("Сгенерированный массив:\n", array);
+
+// Переворачиваем массив
+int[] swappArr = SwapNewArray(array);
+PrintArray("Исходный массив:\n", array);
+PrintArray("Перевернутый массив:\n", swappArr);
+
+SwapArray(array);
+PrintArray("Исходный перевернутый массив:\n", array);
+
+
+//                                                     Методы
+//------------------------------------------------------------------------------------------------------------------
+
+int[] GenerateRandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++) { array[i] = rnd.Next(minValue, maxValue + 1); }
+    return array;
+}
+
+// Вывод массива
+void PrintArray(string msg, int[] array)
+{
+    Console.Write(msg + "[");
+    for (int i = 0; i < array.Length - 1; i++) { Console.Write(array[i] + ", "); }
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+// Перестановка элементов в новом массиве
+int[] SwapNewArray(int[] array)
+{
+    int[] outArr = new int[array.Length];
+    for (int i = 0; i < array.Length; i++) { outArr[array.Length - 1 - i] = array[i]; }
+    return outArr;
+}
+
+// Перестановка элементов в текущем массиве
+void SwapArray(int[] array)
+{
+   // int buferElement = 0;
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        // buferElement = array[array.Length - 1 - i];
+        // array[array.Length - 1 - i] = array[i];
+        // array[i] = buferElement;
+
+        // Вместо использования буферной переменной
+        (array[array.Length-1-i],array[i] )=(array[i], array[array.Length-1-i]); 
+    }
+}
---
+++ Seminar_06_task_45/Program.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------------------------------------------
+//                                                  Задача 44
+//   Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+//------------------------------------------------------------------------------------------------------------------
+
+//                                                Тело программы
+//------------------------------------------------------------------------------------------------------------------
+Console.Clear();
+// Генерация массива из случайных чисел и вывод 
+int[] array = GenerateRandomArray(10, 0, 20);
+PrintArray("Исходный массив:\n", array);
+
+// Копирование массива
+int[] newArray = CopyArray(array);
+PrintArray("Скопированный массив:\n", newArray);
+
+//                                                     Методы
+//------------------------------------------------------------------------------------------------------------------
+// Генерация массива
+int[] GenerateRandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++) { array[i] = rnd.Next(minValue, maxValue + 1); }
+    return array;
+}
+
+// Вывод массива
+void PrintArray(string msg, int[] array)
+{
+    Console.Write(msg +"[");
+    for (int i = 0; i < array.Length - 1; i++) { Console.Write(array[i] + ", "); }
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+// Копирование массива
+int[] CopyArray (int [] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i<array.Length;i++) {newArray[i]=array[i];}
+    return newArray;
+}
