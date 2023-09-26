a8450d3442c841a121352e9dd120c9205d488ebe
draguanna <draguanna@yandex.ru>
2023-09-26 20:44:57 +0300

    Added solutions for tasks 46,48,49,51 from seminar 07

---
+++ Seminar_07_task_49/Program.cs
@@ -0,0 +1,61 @@
+﻿//------------------------------------------------------------------------------------------------------------------
+//                                                   Задача 49     
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+//------------------------------------------------------------------------------------------------------------------
+//                                                 Тело программы
+//------------------------------------------------------------------------------------------------------------------
+Console.Clear();
+int countRows = ReadInt("Введите количество строк: ");
+int countColumn = ReadInt("Введите количество cтолбцов: ");
+int[,] arr2D = Fill2DArray(countRows, countColumn, 0, 10);
+Print2DArray("\nИсходный массив:", arr2D);
+ModifyArray (arr2D);
+Print2DArray("\nМодифицированный массив:", arr2D);
+
+//                                                     Методы
+//------------------------------------------------------------------------------------------------------------------
+// Ввод целого числа с клавиатуры.
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Генерация двумерного массива
+int[,] Fill2DArray(int countRow, int countColumn, int minValue, int maxValue)
+{
+    Random rnd = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rnd.Next(minValue, maxValue);
+        }
+    }
+    return array2D;
+}
+
+// Вывод массива
+void Print2DArray(string msg, int[,] matrix)
+{
+    Console.WriteLine(msg);
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) { Console.Write($"{matrix[i, j],2} "); }
+        Console.WriteLine();
+    }
+}
+
+// Модификация массива
+void ModifyArray (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j+=2)
+        {
+            matrix[i,j] = (int)Math.Pow(matrix[i,j],2);
+        }
+    }
+}
---
+++ Seminar_07_task_51/Program.cs
@@ -0,0 +1,72 @@
+﻿//------------------------------------------------------------------------------------------------------------------
+//                                                   Задача 51                                        
+//    Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами
+//    (0,0); (1;1) и т.д.
+//------------------------------------------------------------------------------------------------------------------
+
+//                                                 Тело программы
+//------------------------------------------------------------------------------------------------------------------
+Console.Clear();
+int countRows = ReadInt("Введите количество строк: ");
+int countColumn = ReadInt("Введите количество cтолбцов: ");
+int[,] arr2D = Fill2DArray(countRows, countColumn, 0, 10);
+Print2DArray("\nИсходный массив:", arr2D);
+int sum = CalculateDiagonalSum(arr2D);
+PrintResult($"суммы элементов главной диагонали: {sum}");
+
+//                                                     Методы
+//------------------------------------------------------------------------------------------------------------------
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Генерация двумерного массива
+int[,] Fill2DArray(int countRow, int countColumn, int minValue, int maxValue)
+{
+    Random rnd = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rnd.Next(minValue, maxValue);
+        }
+    }
+    return array2D;
+}
+
+// Вывод массива
+void Print2DArray(string msg, int[,] matrix)
+{
+    Console.WriteLine(msg);
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) { Console.Write($"{matrix[i, j],2} "); }
+        Console.WriteLine();
+    }
+}
+
+// Подсчет суммы элементов главной диагонали
+int CalculateDiagonalSum(int[,] matrix)
+{
+        int sum = 0;
+        int min = matrix.GetLength(0)>matrix.GetLength(1)
+            ? matrix.GetLength(1)
+            : matrix.GetLength(0);
+
+        for (int i = 0; i <  min ; i++)
+        {
+            sum += matrix[i, i];
+        }
+        return sum;
+}
+
+// Вывод результата 
+void PrintResult (string msg)
+{
+    Console.WriteLine(msg);
+}
