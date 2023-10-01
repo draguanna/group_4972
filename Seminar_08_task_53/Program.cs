5e444fb7e0071efb6163a240661ad9ef39c29ba0
draguanna <draguanna@yandex.ru>
2023-10-01 22:15:24 +0300

    Added for tasks 53, 55, 57, 59 from seminar 08 (fix)

---
+++ Seminar_08_task_53/Program.cs
@@ -0,0 +1,75 @@
+﻿//------------------------------------------------------------------------------------------------------------------
+//                                                   Задача 53                                     
+//   Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+//------------------------------------------------------------------------------------------------------------------
+
+//                                                 Тело программы
+//------------------------------------------------------------------------------------------------------------------
+Console.Clear();
+
+// Ввод парметров матрицы.
+int countRow = ReadInt("Введите количество строк m: ");
+int countCol = ReadInt("Введите количество cтолбцов n: ");
+
+// Генерация и вывод матрицы.
+int minValue = -10; int maxValue = 10;
+int[,] matrix = CreateRandomMatrix(countRow, countCol, minValue, maxValue);
+PrintMatrix(matrix);
+
+// Изменение массива и его вывод
+ChangeMatrix(matrix);
+PrintMatrix(matrix);
+
+//                                                     Методы
+//------------------------------------------------------------------------------------------------------------------
+
+// Ввод целого числа с клавиатуры.
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Генераци двумерного массива, заполненного случайными целыми значениями.
+int[,] CreateRandomMatrix(int countRow, int countCol, int minValue, int maxValue)
+{
+    Random random = new Random();
+    int[,] matrix = new int[countRow, countCol];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countCol; j++)
+        {
+            matrix[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix;
+}
+
+// Вывод матрицы на экран.
+void PrintMatrix(int[,] matrix)
+{   Console.WriteLine();
+
+    int rowCount = matrix.GetLength(0);
+    int colCount = matrix.GetLength(1);
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeMatrix(int [,] matrix)
+{
+    int temp = 0;
+    for (int j = 0; j<matrix.GetLength(1); j++)
+    {
+        temp = matrix[0,j];
+        matrix [0,j] = matrix [matrix.GetLength(0)-1, j];
+        matrix[matrix.GetLength(0)-1, j] = temp;
+    }
+}
---
+++ Seminar_08_task_55/Program.cs
@@ -0,0 +1,95 @@
+﻿//------------------------------------------------------------------------------------------------------------------
+//                                                   Задача 55                                     
+//   Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это
+//   невозможно, программа должна вывести сообщение для пользователя.
+
+//------------------------------------------------------------------------------------------------------------------
+
+//                                                 Тело программы
+//------------------------------------------------------------------------------------------------------------------
+Console.Clear();
+
+// Ввод парметров матрицы.
+int countRow = ReadInt("Введите количество строк m: ");
+int countCol = ReadInt("Введите количество cтолбцов n: ");
+
+// Генерация и вывод матрицы.
+int minValue = 0;
+int maxValue = 100;
+int[,] matrix = CreateRandomMatrix(countRow, countCol, minValue, maxValue);
+PrintMatrix("Исходная матрица", matrix);
+
+// Транспонирование матрицы и вывод результата
+if (CanTranspose(matrix))
+{
+    int[,] transposedMatrix = TransposeMatrix(matrix);
+    PrintMatrix("Матрица после замены строк на столбцы:", transposedMatrix);
+}
+else { Console.WriteLine("\nНевозможно заменить строки на столбцы, так как количество строк и столбцов не совпадает."); }
+
+//                                                     Методы
+//------------------------------------------------------------------------------------------------------------------
+// Ввод целого числа с клавиатуры.
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Генераци двумерного массива, заполненного случайными целыми значениями.
+int[,] CreateRandomMatrix(int countRow, int countCol, int minValue, int maxValue)
+{
+    Random random = new Random();
+    int[,] matrix = new int[countRow, countCol];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countCol; j++)
+        {
+            matrix[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix;
+}
+
+// Вывод двумерного массива на экран.
+void PrintMatrix(string msg, int[,] matrix)
+{
+    Console.WriteLine("\n" + msg);
+
+    int rowCount = matrix.GetLength(0);
+    int colCount = matrix.GetLength(1);
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Проверка может ли быть матрица транспонирована
+bool CanTranspose(int[,] matrix)
+{
+    return matrix.GetLength(0) == matrix.GetLength(1);
+}
+
+// Транспортирование матрицы
+int[,] TransposeMatrix(int[,] matrix)
+{
+    int rowCount = matrix.GetLength(0);
+    int colCount = matrix.GetLength(1);
+
+    int[,] transposedMatrix = new int[colCount, rowCount];
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            transposedMatrix[j, i] = matrix[i, j];
+        }
+    }
+    return transposedMatrix;
+}
